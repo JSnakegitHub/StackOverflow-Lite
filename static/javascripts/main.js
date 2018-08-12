8123a18a77e7a3ee9f1e91a6b60b56498f776e27
JSnakegitHub <nyekowalter69@gmail.com>
2018-08-13 02:08:49 +0300

    I built and linked the question/answers pages [delivers #159639443]

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -15,3 +15,22 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.display = "none";
 }
+
+
+ function showLoginPage() {
+    var x = document.getElementById("myLoginDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showSignUpPage() {
+    var x = document.getElementById("mySignUpDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
