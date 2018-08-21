12b4bffb80d2693c0a29c33ad4c6cc7c598f3eff
JSnakegitHub <nyekowalter69@gmail.com>
2018-08-21 17:08:23 +0300

    API for posting a question

--- api/views.py
+++ api/views.py
@@ -25,3 +25,53 @@ def getOneQuestionById(id):
 
     return jsonify({'Answers' : answersReturned, 'Quetion' :questionReturned })
 
+
+@app.route('/api/v1/question', methods=['POST', 'GET'])
+def postQuestion():
+
+
+    request_data  = request.get_json()
+    if (valid_question(request_data)):
+
+        dictionary_with_highest_question_id = max(allQuestions, key = lambda x:x['id'])
+
+        highest_value_of_id = dictionary_with_highest_question_id['id']
+
+        the_next_value_of_id = highest_value_of_id + 1
+
+        question = {
+                'id' : the_next_value_of_id,
+                'views' : 0,
+                'answers' : 0,
+                'votes' : 0,
+                'title': request_data['title'],
+                'content': request_data['content'],
+                'author': 'Walter Nyeko',
+                'ask_date' : '04-08-2018'
+                
+            }
+        allQuestions.append(question)
+        response = Response("", 201, mimetype="application/json")
+        response.headers['Location'] = "api/v1/question/" + str(request_data['title'])
+        return response
+    else:
+        invalid_entry = {
+            "error": "Invalid question object"
+        }
+        response = Response(json.dumps(invalid_entry), status=400, mimetype="appliation/json")
+        return response
+
+    return jsonify({'Questions' : allQuestions})  
+    
+    
+def valid_question(questionObject):
+    
+    if "title" in questionObject and "content" in questionObject:
+        return True
+    else:
+        return False
+
+
+
+
+
