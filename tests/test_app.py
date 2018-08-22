9376b8e3825dfd7d2c1e45a0f1b7176d74d35ed1
Nyeko Walter <nyekowalter69@gmail.com>
2018-08-22 16:26:12 +0300

    Adding comments on the test methods

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,16 +20,19 @@ def json_of_response(response):
 
 # Test for GET endpoint:
 
+# Testing if the URL for getting all questions is accessible
 def test_get_all_questions_is_successfully_rendered(client):
     response = client.get('/api/v1/questions')
     assert response.status_code == 200
 
+# Testing if the URL for getting one question is accessible
 def test_get_a_question_is_successfully_rendered_for_details_page_with_id(client):
     response = client.get('/api/v1/question/1')
     assert response.status_code == 200
 
 # Test for POST endpoint. Checking resulting json data:
 
+# Testing if the question being sent is JSON formatted
 def test_post_question_is_json_formatted(client):
     response = post_json(client, '/api/v1/question', {
             "id" : 9,
@@ -42,7 +45,7 @@ def test_post_question_is_json_formatted(client):
             "ask_date" : "04-24-2018"})
     assert response.status_code == 400
 
-
+# Testing if the answer being sent is JSON formatted
 def test_post_answer_is_json_formatted(client):
     response = post_json(client, '/api/v1/answers', {
             'id' : 1, 
