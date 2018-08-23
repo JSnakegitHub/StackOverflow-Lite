3c3e65b2a50902a85a136bc2176004339ad649aa
JSnakegitHub <nyekowalter69@gmail.com>
2018-08-24 01:16:39 +0300

    Edited Models To Improve Coverage

--- api/models.py
+++ api/models.py
@@ -1,47 +1,17 @@
-# class Questions:
-#     def __init__(self, question_id, content, title):
-#        self.question_id = question_id
-#        self.content = content
-#        self.title = title
-#        self.answers = []
-
-#     def get_json(self):
-#         questions_json = {
-#             "question_id" : self.question_id,
-#             "content" : self.content,
-#             "title" : self.title
-#             "answers": self.answers
-#         }
-
-#         return questions_json
-
 def Questions():
-
     questions = [
         
     ]
-
     return questions
 
 def Answers():
 
     answers = [
         {
-            # 'id' : 1, 
+            'id' : 1, 
             'question_id' : 1, 
             'content' : "Answer One", 
             'author' : "Walter"
             }
     ]
     return answers
-
-def questionTempId():
-
-    tempID =[
-
-        {
-           "id" : 0
-        }
-    ] 
-
-    return tempID
