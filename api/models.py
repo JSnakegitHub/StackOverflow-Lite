953948e91391ad468cd554bc6d1ab0b2902ef2a5
JSnakegitHub <nyekowalter69@gmail.com>
2018-08-24 01:20:27 +0300

    Edited Models To Improve Coverage

--- api/models.py
+++ api/models.py
@@ -3,15 +3,3 @@ def Questions():
         
     ]
     return questions
-
-def Answers():
-
-    answers = [
-        {
-            'id' : 1, 
-            'question_id' : 1, 
-            'content' : "Answer One", 
-            'author' : "Walter"
-            }
-    ]
-    return answers
